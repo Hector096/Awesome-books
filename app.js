09f652e443f95eb2ca8c8a6f1191fac12ca1a05a
Hector <vv96rca@gmail.com>
2021-09-13 18:49:03 +0530

    resolved linting errors

--- app.js
+++ app.js
@@ -1,47 +1,46 @@
 let books = [];
 
-let booksContainer = document.getElementById('books');
-let addBtn = document.getElementById('add-btn');
-let title = document.getElementById('title');
-let author = document.getElementById('author');
+const booksContainer = document.getElementById('books');
+const addBtn = document.getElementById('add-btn');
+const title = document.getElementById('title');
+const author = document.getElementById('author');
 
-if(JSON.parse(localStorage.getItem("books")).length != 0) {
-  books = [...books, ...JSON.parse(localStorage.getItem("books"))]
+if (JSON.parse(localStorage.getItem('books')).length !== 0) {
+  books = [...books, ...JSON.parse(localStorage.getItem('books'))];
 }
+
+const setItems = () => {
+  booksContainer.innerHTML = '';
+  books.forEach((book) => {
+    const li = document.createElement('li');
+    const h2 = document.createElement('h2');
+    h2.textContent = book.title;
+    const h3 = document.createElement('h3');
+    h3.textContent = book.author;
+    const button = document.createElement('button');
+    button.textContent = 'X';
+    button.addEventListener('click', () => {
+      const newBooks = books.filter((books) => books.title !== book.title);
+      books = [...newBooks];
+      localStorage.setItem('books', JSON.stringify(books));
+      setItems();
+    });
+    li.appendChild(h2);
+    li.appendChild(h3);
+    li.appendChild(button);
+    return booksContainer.appendChild(li);
+  });
+};
 
 addBtn.addEventListener('click', () => {
   books.push({
     title: title.value,
-    author: author.value
+    author: author.value,
   });
-  localStorage.setItem("books", JSON.stringify(books));
+  localStorage.setItem('books', JSON.stringify(books));
   title.value = '';
   author.value = '';
   setItems();
 });
 
-const removeBook = (key, index) => {
-  var newBooks = books.filter(books => books.title != key);
-  books = [...newBooks]
-  localStorage.setItem("books", JSON.stringify(books));
-  setItems();
-}
-
-setItems = () => {
-    booksContainer.innerHTML = '';
-  books.forEach((book, index) => {
-    let li = document.createElement('li');
-    let h2 = document.createElement('h2');
-    h2.textContent = book.title
-    let h3 = document.createElement('h3');
-    h3.textContent = book.author
-    let button = document.createElement('button');
-    button.textContent = 'X';
-    button.addEventListener('click', () => removeBook(book.title, index));
-    li.appendChild(h2);
-    li.appendChild(h3);
-    li.appendChild(button);
-    return booksContainer.appendChild(li)
-  })
-}
 setItems();
