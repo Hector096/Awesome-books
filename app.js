bac0aeefbe1445de63369a0bab74f97016bf1ecb
AhmedOmarAL-farouq <ahmed.omar.alfarouq@gmail.com>
2021-09-13 14:48:51 +0200

    add remove logic

--- app.js
+++ app.js
@@ -1,12 +1,49 @@
 let books = [];
 
+let booksContainer = document.getElementById('books');
+let addBtn = document.getElementById('add-btn');
+let title = document.getElementById('title');
+let author = document.getElementById('author');
 
-const addBook = (title,author) =>{
-    books.push({"title": title,"author":author});
-    localStorage.setItem("books",books);
+if(JSON.parse(localStorage.getItem("books")).length != 0) {
+  books = [...books, ...JSON.parse(localStorage.getItem("books"))]
 }
 
+addBtn.addEventListener('click', () => {
+  books.push({
+    title: title.value,
+    author: author.value
+  });
+  localStorage.setItem("books", JSON.stringify(books));
+  title.value = '';
+  author.value = '';
+  setItems();
+});
 
-addBook("Nolan","Chris");
-addBook("Omar","Chris");
-console.log( "title" in books[1]);
+const removeBook = (key, index) => {
+  var newBooks = books.filter(books => books.title != key);
+  books = [...newBooks]
+  booksContainer.removeChild(booksContainer.childNodes[index]);
+  localStorage.setItem("books", JSON.stringify(books));
+}
+
+setItems = () => {
+  for (let i = 0; i < booksContainer.children.length; i++) {
+    booksContainer.removeChild(booksContainer.children[i])
+  }
+  books.forEach((book, index) => {
+    let li = document.createElement('li');
+    let h2 = document.createElement('h2');
+    h2.textContent = book.title
+    let h3 = document.createElement('h3');
+    h3.textContent = book.author
+    let button = document.createElement('button');
+    button.textContent = 'X';
+    button.addEventListener('click', () => removeBook(book.title, index));
+    li.appendChild(h2);
+    li.appendChild(h3);
+    li.appendChild(button);
+    return booksContainer.appendChild(li)
+  })
+}
+setItems();
