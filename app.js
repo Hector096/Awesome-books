f28ded00eb007c2e81613a8b7714e359704ae21a
Hector <vv96rca@gmail.com>
2021-09-16 14:15:56 +0530

    added style for books container and resolved linting errors

--- app.js
+++ app.js
@@ -26,29 +26,37 @@ class Book {
 
 const setItems = () => {
   booksContainer.innerHTML = '';
-  books.forEach((book) => {
-    const li = document.createElement('li');
-    li.className = 'row';
-    const h2 = document.createElement('h2');
-    h2.textContent = book.title;
-    h2.className = 'col';
-    const h3 = document.createElement('h3');
-    h3.textContent = book.author;
-    h3.className = 'col';
-    const button = document.createElement('button');
-    button.className = 'btn btn-danger m-3 col-2';
-    button.textContent = 'Remove';
-    button.addEventListener('click', () => {
-      const bookToRemove = new Book(book.title, book.author);
-      bookToRemove.removeBook();
-      localStorage.setItem('books', JSON.stringify(books));
-      setItems();
+  if (books.length !== 0) {
+    books.forEach((book) => {
+      const li = document.createElement('li');
+      li.className = 'row';
+      const h2 = document.createElement('h2');
+      h2.textContent = book.title;
+      h2.className = 'col';
+      const h3 = document.createElement('h3');
+      h3.textContent = book.author;
+      h3.className = 'col';
+      const button = document.createElement('button');
+      button.className = 'btn btn-danger m-3 col-2';
+      button.textContent = 'Remove';
+      button.addEventListener('click', () => {
+        const bookToRemove = new Book(book.title, book.author);
+        bookToRemove.removeBook();
+        localStorage.setItem('books', JSON.stringify(books));
+        setItems();
+      });
+      li.appendChild(h2);
+      li.appendChild(h3);
+      li.appendChild(button);
+      return booksContainer.appendChild(li);
     });
-    li.appendChild(h2);
-    li.appendChild(h3);
-    li.appendChild(button);
-    return booksContainer.appendChild(li);
-  });
+  } else {
+    const h5 = document.createElement('h5');
+    h5.textContent = 'No Books Found';
+    h5.className = 'text-center';
+    return booksContainer.appendChild(h5);
+  }
+  return null;
 };
 
 const getUuid = () => new Date().getTime().toString() + Math.floor(Math.random() * 1000000);
